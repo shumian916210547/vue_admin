222755803f04bcd441ff7a0ea9d2e83da2923255
shumian <916210547@qq.com>
2022-05-07 13:41:05 +0800

    修海配置

---
+++ src/http/index.js
@@ -0,0 +1,131 @@
+import axios from "axios";
+
+import { useRouter } from "vue-router";
+const router = useRouter();
+
+axios.defaults.timeout = 5000;
+axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;
+
+//http request 拦截器
+axios.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {}
+);
+
+//http response 拦截器
+axios.interceptors.response.use(
+  (response) => {
+    if (response) {
+      if (response.data.code == 200) {
+        return response;
+      } else {
+      }
+    } else {
+    }
+  },
+  (error) => {}
+);
+
+/**
+ * 封装get方法
+ * @param url
+ * @param data
+ * @returns {Promise}
+ */
+
+export function get(url, params = {}) {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(url, {
+        params: params,
+      })
+      .then((response) => {
+        resolve(response?.data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
+
+/**
+ * 封装post请求
+ * @param url
+ * @param data
+ * @returns {Promise}
+ */
+
+export function post(url, data = {}) {
+  return new Promise((resolve, reject) => {
+    axios.post(url, data).then(
+      (response) => {
+        resolve(response.data);
+      },
+      (err) => {
+        reject(err);
+      }
+    );
+  });
+}
+
+/**
+ * 封装patch请求
+ * @param url
+ * @param data
+ * @returns {Promise}
+ */
+
+export function patch(url, data = {}) {
+  return new Promise((resolve, reject) => {
+    axios.patch(url, data).then(
+      (response) => {
+        resolve(response.data);
+      },
+      (err) => {
+        reject(err);
+      }
+    );
+  });
+}
+
+/**
+ * 封装put请求
+ * @param url
+ * @param data
+ * @returns {Promise}
+ */
+
+export function put(url, data = {}) {
+  return new Promise((resolve, reject) => {
+    axios.put(url, data).then(
+      (response) => {
+        resolve(response.data);
+      },
+      (err) => {
+        reject(err);
+      }
+    );
+  });
+}
+
+/**
+ * 封装delete请求
+ * @param url
+ * @param data
+ * @returns {Promise}
+ */
+
+export function remove(url, data) {
+  return new Promise((resolve, reject) => {
+    axios.delete(url, { data: data }).then(
+      (response) => {
+        resolve(response.data);
+      },
+      (err) => {
+        reject(err);
+      }
+    );
+  });
+}
