9c5ceef2d941ff24a387a55c6ca67b7505aa77f5
shumian <916210547@qq.com>
2022-05-07 13:23:15 +0800

    创建项目

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const URL = process.env.VUE_APP_BASE_URL;
+const API = process.env.VUE_APP_BASE_API;
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    hot: true,
+    liveReload: true,
+    host: "localhost",
+    port: 4200,
+    open: false,
+    proxy: {
+      URL: {
+        target: API,
+        pathRewrite: {
+          ["^" + URL]: "",
+        },
+      },
+    },
+  },
+});
